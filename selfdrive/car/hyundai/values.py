95833ff0dcf70881762b1c215fd19c603181769f
Bruce Wayne <harald.the.engineer@gmail.com>
2022-12-11 21:53:46 -0800

    Revert "fix thneed runner"
    This reverts commit 3736a3e00eab12a0b29feb83bba2455d01d4abec.

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -25,7 +25,7 @@ class CarControllerParams:
     self.STEER_THRESHOLD = 150
 
     if CP.carFingerprint in CANFD_CAR:
-      self.STEER_MAX = 330
+      self.STEER_MAX = 270
       self.STEER_DRIVER_ALLOWANCE = 350
       self.STEER_DRIVER_MULTIPLIER = 2
       self.STEER_THRESHOLD = 350
