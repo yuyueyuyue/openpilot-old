2cbc38f7b99b1a9dfb64f1be2636fd68611217e0
yuyueyuyue <42679741+yuyueyuyue@users.noreply.github.com>
2023-06-07 11:02:18 -0500

    Update interface.py

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -104,12 +104,8 @@ class CarInterface(CarInterfaceBase):
     elif candidate == CAR.ATLAS_MK1:
       ret.mass = 2011 + STD_CARGO_KG
       ret.wheelbase = 2.98
-      ret.vEgoStarting = 0.25
-      ret.vEgoStopping = 0.25
-      ret.stoppingDecelRate = 0.6
+      ret.vEgoStopping = 0.35
       ret.longitudinalActuatorDelayUpperBound = 0.5
-      ret.longitudinalTuning.kpV = [0.3]
-      ret.longitudinalTuning.kiV = [0.1]
 
     elif candidate == CAR.CRAFTER_MK2:
       ret.mass = 2100 + STD_CARGO_KG
